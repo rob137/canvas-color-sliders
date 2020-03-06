197d4faaa137ad24531052d26b5c6b0d484a5cb5
rob <robertaxelkirby@gmail.com>
2020-03-06 11:20:30 +0000

    Replace useEffect with useReducer (just for red)

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import BrightnessSlider from "./components/BrightnessSlider";
 import ColorSlider from "./components/ColorSlider";
 
@@ -11,53 +11,69 @@ const getOriginalPixels = (canvas, image) => {
   }
   return [];
 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "initial":
+      return { currentPixels: action.payload };
+    case "color":
+      const { color, value, originalPixels } = action.payload;
+      const colors = ["red", "green", "blue"];
+      const colorIndex = colors.indexOf(color);
+      const pixels = Uint8ClampedArray.from(originalPixels);
+      for (let p = colorIndex; p < pixels.length; p += 4) {
+        pixels[p] = originalPixels[p] + value;
+      }
+      return { currentPixels: pixels };
+    case "brightness":
+      return "placeholder";
+    default:
+      throw new Error();
+  }
+};
+
+function fromCanvas(canvasRef) {
+  const canvas = canvasRef.current;
+  const [w, h] = [canvas.width, canvas.height];
+  const context = canvas.getContext("2d");
+  const imageData = context.createImageData(w, h);
+  const pixels = imageData.data;
+  return { canvas, w, h, context, imageData, pixels };
+}
+
 export default () => {
   const [originalPixels, setOriginalPixels] = useState([]);
-  const [brightness, setBrightness] = useState(1);
   const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
   const canvasRef = useRef(null);
-  const colorDict = { red, green, blue };
+  const initialState = { currentPixels: [] };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    let image = new Image();
+    const image = new Image();
     image.onload = () => {
       const op = getOriginalPixels(canvasRef.current, image);
       setOriginalPixels(op);
+      dispatch({ type: "initial", payload: op });
     };
     image.crossOrigin = "Anonymous";
     image.src = "tubulin.png";
   }, []);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const colors = ["red", "green", "blue"];
-    const [w, h] = [canvas.width, canvas.height];
-    const context = canvas.getContext("2d");
-    const imageData = context.createImageData(w, h);
-    let pixels = imageData.data;
-
-    // Apply colors
-    colors.forEach(color => {
-      const index = colors.indexOf(color);
-      for (let p = index; p < pixels.length; p += 4) {
-        pixels[p] = originalPixels[p] + colorDict[color];
-      }
-    });
-    // Apply brightness
+    const { canvas, w, h, context, imageData, pixels } = fromCanvas(canvasRef);
     for (let p = 0; p < pixels.length; p++) {
-      if (p % 4 !== 3) {
-        pixels[p] = pixels[p] + brightness;
-      } else {
-        pixels[p] = originalPixels[p];
-      }
+      pixels[p] = state.currentPixels[p];
     }
+    context.putImageData(imageData, 0, 0);
+  }, [red]);
 
-    canvas.getContext("2d").putImageData(imageData, 0, 0);
-  }, [brightness, red, green, blue]);
-
-  const updateImage = (canvas, originalPixels, value, color) => {};
+  const applyColor = (color, value) => {
+    const { canvas, w, h, context, imageData, pixels } = fromCanvas(canvasRef);
+    dispatch({
+      type: "color",
+      payload: { color, value, originalPixels }
+    });
+  };
 
   return (
     <>
@@ -68,32 +84,16 @@ export default () => {
         height="600"
         style={{ border: "1px solid red" }}
       />
-      <BrightnessSlider
-        callback={v => {
-          updateImage(canvasRef.current, originalPixels, v, "brightness");
-          setBrightness(v);
-        }}
-      />
+      <BrightnessSlider callback={v => {}} />
       <ColorSlider
         callback={v => {
           setRed(v);
+          applyColor("red", v);
         }}
         color={"red"}
       />
-      <ColorSlider
-        callback={v => {
-          updateImage(canvasRef.current, originalPixels, v, "green");
-          setGreen(v);
-        }}
-        color={"green"}
-      />
-      <ColorSlider
-        callback={v => {
-          updateImage(canvasRef.current, originalPixels, v, "blue");
-          setBlue(v);
-        }}
-        color={"blue"}
-      />
+      <ColorSlider callback={v => {}} color={"green"} />
+      <ColorSlider callback={v => {}} color={"blue"} />
     </>
   );
 };
